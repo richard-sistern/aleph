5b1c4643b9ad76de25d0ee2a52f1aa06b520230f
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-05-24 15:21:35 +0100

    Add SQL to set database compatability level

---
+++ sql-server/set-compatibility-level.sql
@@ -0,0 +1,8 @@
+--Sets Transact-SQL and query processing behaviors to be compatible with the specified version of the SQL engine.
+-- https://docs.microsoft.com/en-us/sql/t-sql/statements/alter-database-transact-sql-compatibility-level?view=sql-server-ver15
+
+-- SQL Server 2019
+ALTER DATABASE database_name
+SET COMPATIBILITY_LEVEL = 150
+
+
